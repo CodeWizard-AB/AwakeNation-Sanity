1b549718df6aa6af8ec31a94232d25841fe9d5f3
Anunay Argha <anunayargha@gmail.com>
2025-08-18 23:31:38 +0600

    fix: update testimonial schema

--- schemaTypes/testimonial.ts
+++ schemaTypes/testimonial.ts
@@ -1,3 +1,4 @@
+// schemas/testimonial.js
 import { defineField, defineType } from "sanity";
 
 const testimonial = defineType({
@@ -6,11 +7,55 @@ const testimonial = defineType({
 	type: "document",
 	fields: [
 		defineField({
-			name: "name",
-			title: "Name",
+			name: "authorName",
+			title: "Author Name",
 			type: "string",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "role",
+			title: "Role or Title",
+			type: "string",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "testimonialText",
+			title: "Testimonial Text",
+			type: "text",
+			validation: (Rule) => Rule.required(),
+		}),
+		defineField({
+			name: "authorPhoto",
+			title: "Author Photo",
+			type: "image",
+			validation: (Rule) => Rule.required(),
+			options: {
+				hotspot: true,
+			},
+			fields: [
+				defineField({
+					name: "alt",
+					title: "Alternative Text",
+					type: "string",
+					description: "Important for accessibility and SEO.",
+				}),
+			],
 		}),
 	],
+	preview: {
+		select: {
+			title: "authorName",
+			subtitle: "role",
+			media: "authorPhoto",
+		},
+		prepare({ title, subtitle, media }) {
+			return {
+				title,
+				subtitle,
+				media,
+			};
+		},
+	},
 });
 
 export default testimonial;
